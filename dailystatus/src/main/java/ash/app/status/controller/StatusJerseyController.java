a71bddc7b541209b58fd4df304e2c3097feda0ff
ashpatz <007.ashish@gmail.com>
2015-09-19 01:16:34 +0530

    First draft of Angular + Spring + Jersey

--- dailystatus/src/main/java/ash/app/status/controller/StatusJerseyController.java
+++ dailystatus/src/main/java/ash/app/status/controller/StatusJerseyController.java
@@ -1,19 +1,57 @@
 package ash.app.status.controller;
 
 import ash.app.status.model.Employee;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Component;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.GenericEntity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Sentinel on 9/17/2015.
  */
-@Path("/status")
+@Component
+@Path("/employee")
 public class StatusJerseyController {
 
-    public @ResponseBody
-    List<Employee> getEmployees(){
-        return null;
+    @GET
+    @Path("/fetch")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getEmployees(){
+        List<Employee> employeeList = new ArrayList<>();
+        Employee employee = new Employee();
+        employee.setEmployeeId(1);
+        employee.setName("Ashish Patil");
+        employee.setDesignation("Architect");
+        employee.setCity("Switzerland");
+        employeeList.add(employee);
+
+        employee = new Employee();
+        employee.setEmployeeId(2);
+        employee.setName("Ramadhir Singh");
+        employee.setDesignation("Mukhiya");
+        employee.setCity("Wasseypur");
+        employeeList.add(employee);
+
+       // GenericEntity<List<Employee>> jsonEmployeeList = new GenericEntity<List<Employee>>(employeeList){};
+        return Response.ok(employeeList).build();
+    }
+
+    @GET
+    @Path("/fetchone")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response getEmployee(){
+        Employee employee = new Employee();
+        employee.setEmployeeId(1);
+        employee.setName("Ashish Patil");
+        employee.setDesignation("Architect");
+        employee.setCity("Switzerland");
+
+        return Response.ok(employee).build();
     }
 }
--- dailystatus/src/main/webapp/js/myController.js
+++ dailystatus/src/main/webapp/js/myController.js
@@ -1,8 +1,8 @@
 angular.module('myApp', [])
         .controller('myController', function($scope, $http){
-        $http.get("http://localhost:8080/dailystatus/status/get")
+        $http.get("http://localhost:8080/dailystatus/rest/employee/fetch")
             .success(function(response){
-                $scope.myVar = response.key;
+                $scope.myVar = response[1].name;
             });
         $scope.getCharges = function(){
             return 350;
