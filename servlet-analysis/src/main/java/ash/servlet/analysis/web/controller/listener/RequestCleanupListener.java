02e04bc0d0c181683a790e15414e3f452c8905c2
ashpatz <007.ashish@gmail.com>
2020-01-23 13:55:39 -0800

    Renaming class

--- servlet-analysis/src/main/java/ash/servlet/analysis/web/controller/listener/RequestCleanupListener.java
+++ servlet-analysis/src/main/java/ash/servlet/analysis/web/controller/listener/RequestCleanupListener.java
@@ -3,7 +3,7 @@ package ash.servlet.analysis.web.controller.listener;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 
-public class RequestCleanupFilter implements ServletRequestListener {
+public class RequestCleanupListener implements ServletRequestListener {
     @Override
     public void requestDestroyed(ServletRequestEvent event) {
 
